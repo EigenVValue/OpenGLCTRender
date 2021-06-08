cd7de162aefe5875028a4ec36e9061e50e3c3a1e
EigenVValue <david1215035822@gmail.com>
2021-06-08 16:03:41 +0800

    change some struct

---
+++ controlsForFOV.cpp
@@ -0,0 +1,117 @@
+// Include GLFW
+#include <GLFW/glfw3.h>
+extern GLFWwindow* window; 
+
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "controlsForFOV.hpp"
+
+glm::mat4 ViewMatrix;
+glm::mat4 ProjectionMatrix;
+// Model Matrix; constant
+glm::vec3 modelPosition = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 modelRotation = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 modelScaling = glm::vec3(0.03f, 0.03f, 0.03f);
+
+glm::mat4 getViewMatrix() {
+	return ViewMatrix;
+}
+glm::mat4 getProjectionMatrix() {
+	return ProjectionMatrix;
+}
+glm::vec3 getModelPosition() {
+	return modelPosition;
+}
+glm::vec3 getModelRotation() {
+	return modelRotation;
+}
+glm::vec3 getModelScaling() {
+	return modelScaling;
+}
+
+// Initial position & up:
+glm::vec4 position = glm::vec4(0, 0, 15, 1);
+glm::vec4 up = glm::vec4(0, 2, 15, 1);
+// Initial horizontal angle : none
+float horizontalAngle = 0.0f;
+// Initial vertical angle : none
+float verticalAngle = 0.0f;
+// Initial Field of View
+float initialFoV = 45.0f;
+
+float mouseSpeed = 0.001f;
+bool click = false;
+
+void computeMatricesFromInputs(const int WIDTH, const int HEIGHT) {
+	// glfwGetTime is called only once, the first time this function is called
+	static double lastTime = glfwGetTime();
+
+	// Compute time difference between current and last frame
+	double currentTime = glfwGetTime();
+	float deltaTime = float(currentTime - lastTime);
+
+	// Get mouse position
+	double xpos, ypos;
+	glfwGetCursorPos(window, &xpos, &ypos);
+
+	// Reset mouse position for next frame
+	glfwSetCursorPos(window, WIDTH / 2, HEIGHT / 2);
+
+	// Tow mouse event
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetMouseButtonCallback(window, mouse_callback);
+
+	// Rotate
+	if (click) {
+		// Compute new orientation
+		horizontalAngle = -mouseSpeed * float(WIDTH / 2 - xpos);
+		verticalAngle = -mouseSpeed * float(HEIGHT / 2 - ypos);
+
+		// Rotate at Y-aixs
+		glm::mat4 rotationY = glm::mat4(1.0f);
+		rotationY[0][0] = glm::cos(horizontalAngle);
+		rotationY[0][2] = -1 * glm::sin(horizontalAngle);
+		rotationY[2][0] = glm::sin(horizontalAngle);
+		rotationY[2][2] = glm::cos(horizontalAngle);
+		position = position * rotationY;
+		up = up * rotationY;
+
+		// Rotate at X-aixs
+		glm::mat4 rotationX = glm::mat4(1.0f);
+		rotationX[1][1] = glm::cos(verticalAngle);
+		rotationX[1][2] = glm::sin(verticalAngle);
+		rotationX[2][1] = -1 * glm::sin(verticalAngle);
+		rotationX[2][2] = glm::cos(verticalAngle);
+		position = position * rotationX;
+		up = up * rotationX;
+	}
+
+	// Projection matrix : 45?Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	ProjectionMatrix = glm::perspective(glm::radians(45.0f), float(WIDTH) / float(HEIGHT), 0.1f, 100.0f);
+	// Camera matrix
+	ViewMatrix = glm::lookAt(
+		glm::vec3(position.x, position.y, position.z),	// Camera is here
+		glm::vec3(0, 0, 0),											// Looks at origin(0,0,0)
+		glm::vec3(up.x, up.y, up.z)								// Head is up (set to 0,-1,0 to look upside-down)
+	);
+
+	// For the next frame, the "last time" will be "now"
+	lastTime = currentTime;
+}
+
+void mouse_callback(GLFWwindow* window, int button, int action, int mods) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		click = true;
+	}
+	else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+		click = false;
+	}
+}
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+	// Scale the coordinate of position
+	position.x *= 1 - yoffset * 0.05;
+	position.y *= 1 - yoffset * 0.05;
+	position.z *= 1 - yoffset * 0.05;
+}
---
+++ loadOBJ.cpp
@@ -0,0 +1,92 @@
+#include <vector>
+#include <stdio.h>
+#include <iostream>
+
+#include <glm/glm.hpp>
+
+#include "loadOBJ.hpp"
+
+/**
+ * OBJ loader
+ * Only vertices and their indices, no noramls and UVs.
+ * Because obj export from fiji only has its vertices and faces.
+ */
+
+bool loadOBJ(const char* path, std::vector<glm::vec3> & objVertices,
+	std::vector<unsigned int> & objFaces) {
+	printf("Loading OBJ file %s...\n", path);
+
+	FILE * file;
+	fopen_s(&file, path, "r");
+	if (file == NULL) {
+		printf("Impossible to open the file !\n");
+		getchar();
+		return false;
+	}
+
+	while (1) {
+		char lineHeader[128];
+		// read the first word of the line
+		int res = fscanf_s(file, "%c", &lineHeader);
+		if (res == EOF)
+			break;
+		// else : parse lineHeader
+
+		if (strcmp(lineHeader, "v") == 0) {
+			// Read line which header is v
+			glm::vec3 vertex;
+			fscanf_s(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			objVertices.push_back(vertex);
+		} else if (strcmp(lineHeader, "f") == 0) {
+			// Read line which header is f
+			unsigned int face[3];
+			int matches = fscanf_s(file, "%ld %ld %ld\n", &face[0], &face[1], &face[2]);
+			if (matches != 3) {
+				printf("File can't be read by our simple parser\n");
+				fclose(file);
+				return false;
+			}
+			objFaces.push_back(face[0]);
+			objFaces.push_back(face[1]);
+			objFaces.push_back(face[2]);
+		} else {
+			// Probably a comment, eat up the rest of the line
+			char stupidBuffer[1000];
+			fgets(stupidBuffer, 1000, file);
+		}
+	} // Loop ends
+	
+	fclose(file);
+	printf("End Loading...\n");
+	return true;
+}
+
+void objVerticesToGLVertices(std::vector<glm::vec3> & out_vertices,
+	const std::vector<glm::vec3> & objVertices,
+	const std::vector<unsigned int> & objFaces) {
+
+	float pivot[3] = { 0.0f };
+
+	// For each vertex of each triangle
+	for (unsigned int i = 0; i < objFaces.size(); i++) {
+		// Get the indices of its attributes
+		// Get the attributes thanks to the index
+		glm::vec3 vertex = objVertices[objFaces[i] - 1];
+		out_vertices.push_back(vertex);
+
+		// Add up
+		pivot[0] += vertex.x;
+		pivot[1] += vertex.y;
+		pivot[2] += vertex.z;
+	}
+	// Get pivot
+	pivot[0] /= out_vertices.size();
+	pivot[1] /= out_vertices.size();
+	pivot[2] /= out_vertices.size();
+
+	for (auto & vertex : out_vertices) {
+		vertex.x -= pivot[0];
+		vertex.y -= pivot[1];
+		vertex.z -= pivot[2];
+	}
+}
